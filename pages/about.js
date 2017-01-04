b745f7ea064eaddeb7d833a792ed4da5cb3b51d9
ShivamDh <shdh8888@yahoo.ca>
2017-01-04 00:49:54 -0500

    A new page that will include information about me later

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class ReactComponent extends React.Component {
+
+  render () {
+    return (
+      <div>
+        <h1> Its a page about me </h1>
+      </div>
+    )
+  }
+}
