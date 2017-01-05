773cd915b29acae7acb6f5db077cf56682349e24
ShivamDh <shdh8888@yahoo.ca>
2017-01-04 21:36:02 -0500

    New header updates

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -13,6 +13,9 @@ module.exports = React.createClass({
       children: React.PropTypes.any,
     }
   },
+	// Change link to 'shivamdh.github.io/Personal-Website/' when publish to Github
+	// Change link to 'shivamdh.github.io/Personal-Website/about/' when publish to Github
+	// Change link to 'shivamdh.github.io/Personal-Website/projects/' when publish to Github
   render () {
     return (
       <div>
@@ -31,23 +34,25 @@ module.exports = React.createClass({
               padding: `${rhythm(1)} ${rhythm(3/4)}`,
             }}
           >
+		  
             <Link to={prefixLink('/')} style={{ textDecoration: 'none'}}>
 				<p className="HomeNav">
 					Home
 				</p>
             </Link>
+			<Link to={prefixLink('/about/')} style={{ textDecoration: 'none'}}>
+				<p className="AboutMeNav">
+					About Me
+				</p>
+            </Link>
+			<Link to={prefixLink('/projects/')} style={{ textDecoration: 'none'}}>
+				<p className="ProjNav">
+					My Projects
+				</p>
+            </Link>
           </Container>
         </Headroom>
-		<Container
-          style={{
-            maxWidth: 960,
-            padding: `${rhythm(1)} ${rhythm(3/4)}`,
-            paddingTop: 0,
-			backgroundColor: "grey",
-          }}
-        >
-		  {this.props.children}
-        </Container>
+		{this.props.children}
       </div>
     )
   },
