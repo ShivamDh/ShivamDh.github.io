57ce63d58248e2acc6db275547e51b0aa1165a53
ShivamDh <shdh8888@yahoo.ca>
2017-01-08 14:52:08 -0500

    Homepage reshuffled, added picture of me

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ import { config } from 'config'
 export default class Index extends React.Component {
 	// Change link to 'shivamdh.github.io/Personal-Website/about/' when publish to Github
 				// <img src = "http://wallpaper-gallery.net/images/technology-wallpaper/technology-wallpaper-18.jpg"/>
+  
   render () {
     return (
       <div>
@@ -19,24 +20,29 @@ export default class Index extends React.Component {
             {"name": "keywords", "content": "sample, something"},
           ]}
         />
-			<div id="BottomofPg">
-				<p id="About">
-					A
-				</p>
+			<div id="ScrollDown">
+				<a href="#next"> <img src = "https://raw.githubusercontent.com/ShivamDh/Personal-Website/gh-pages/down_arrow.png" /> </a>
 			</div>
 			
 			<div className = "HomeBack"> 
-				<img src = "/background.jpg"/>
+				<img src = "https://raw.githubusercontent.com/ShivamDh/Personal-Website/gh-pages/background.jpg"/>
 			</div>
 			<div className = "Welcome">
-				<h1> Welcome to my website </h1>
+				<pre className="tab">
+					<p>     Shivam      Dharme </p>
+				</pre>
 			</div>
-			<div className = "Scroll">
-				<img src = "http://www.pngmart.com/files/3/Down-Arrow-PNG-Pic.png"/>
+			<div id="pic">
+				<img src = "https://raw.githubusercontent.com/ShivamDh/Personal-Website/gh-pages/4426.jpg"/>
+				<p> The will to win, the desire to succeed, the urge to reach your full potential ... these are the keys that will unlock the door to personal excellence.
+				- Confucius </p>
 			</div>
-			<div className = "ScrollText"><h3>Scroll Down</h3></div>
-			<div className = "JustMe">
 			
+			<a name = "next"> </a>
+			<div className = "JustMe">
+				<p> Hardworking. Inquisitive. Motivated.</p>
+				<p> Hello </p>
+				<p> Hello </p>
 			</div>
 			<br/>
 			<br/>
@@ -60,7 +66,6 @@ export default class Index extends React.Component {
 			<br/>			
 			<br/>			
 			<br/>			
-			Hello
       </div>
     )
   }
