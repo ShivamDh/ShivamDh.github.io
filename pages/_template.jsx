30f7ab6c4add74fedb518a05ad7611879f9fb3aa
ShivamDh <shdh8888@yahoo.ca>
2017-01-07 23:59:57 -0500

    New signature added and no loading page

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -3,26 +3,27 @@ import { Container } from 'react-responsive-grid'
 import { Link } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
 import Headroom from 'react-headroom'
+import $ from 'jquery'
 import '../css/styles'
 
 import { rhythm } from '../utils/typography'
 
 module.exports = React.createClass({
-  loadingtext () {
-  	var winH = window.innerHeight;
-	var winW = window.innerWidth;
-	return (
-		<div id="Load">
-			<p style={{marginTop: winH/2, marginLeft: winW/2-275}}id= "Loading"> Some stuff I sayYYYY </p>
-		</div>
-	  )
+  
+  componentDidMount () {
+	  window.addEventListener('resize', () => this.forceUpdate())
   },
   
-  loading() {
-	  window.addEventListener('load', function() {
-		var loader = document.getElementById('LoadingScreen');
-		loader.style.display = 'none';
-	  })
+  welcome() {
+	
+	var check = window.innerWidth;
+	if (check > 1200) {
+		return (
+			<div>
+			<p className="Signature">MAVIHS</p>
+			</div>
+		)
+	}
   },
   
   propTypes () {
@@ -38,10 +39,8 @@ module.exports = React.createClass({
 	// Change link to 'shivamdh.github.io/Personal-Website/contact/' when publish to Github
   render () {
     return (
-      <div>
-        <div id="LoadingScreen"> {this.loadingtext()} {this.loading()} </div>
-
-        <Headroom
+	<div>
+		<Headroom
           wrapperStyle={{
             marginBottom: rhythm(1),
           }}
@@ -52,27 +51,27 @@ module.exports = React.createClass({
             }}
           >
 		  
-            <Link to={prefixLink('/')} style={{ textDecoration: 'none'}}>
+            <Link to={prefixLink('shivamdh.github.io/Personal-Website/')} style={{ textDecoration: 'none'}}>
 				<p className="HomeNav">
 					Home
 				</p>
             </Link>
-			<Link to={prefixLink('/about/')} style={{ textDecoration: 'none'}}>
+			<Link to={prefixLink('shivamdh.github.io/Personal-Website/about/')} style={{ textDecoration: 'none'}}>
 				<p className="AboutMeNav">
 					About Me
 				</p>
             </Link>
-			<Link to={prefixLink('/projects/')} style={{ textDecoration: 'none'}}>
+			<Link to={prefixLink('shivamdh.github.io/Personal-Website/projects/')} style={{ textDecoration: 'none'}}>
 				<p className="ProjNav">
 					My Projects
 				</p>
 			</Link>
-			<Link to={prefixLink('/coop/')} style={{ textDecoration: 'none'}}>
+			<Link to={prefixLink('shivamdh.github.io/Personal-Website/coop/')} style={{ textDecoration: 'none'}}>
 				<p className="CoopNav">
 					Co-op
 				</p>
             </Link>
-			<Link to={prefixLink('/contact/')} style={{ textDecoration: 'none'}}>
+			<Link to={prefixLink('shivamdh.github.io/Personal-Website/contact/')} style={{ textDecoration: 'none'}}>
 				<p className="ContactNav">
 					Contact Me
 				</p>
@@ -80,13 +79,8 @@ module.exports = React.createClass({
           </Container>
         </Headroom>
 		{this.props.children}
-		<img src="http://kingofwallpapers.com/high-resolution-pictures/high-resolution-pictures-007.jpg"/>
-		<img src="http://www.graphicsgodown.com/downloads/000006.jpg"/>
-		<img src="http://www.amazingwallpaperz.com/wp-content/uploads/Battlefield-4-Wallpaper-High-Quality.jpg"/>
-		<img src="https://static.pexels.com/photos/28221/pexels-photo-28221.jpg"/>
       </div>
     )
   },
 })
-
 //Random high-quality pictures are added at the end to slow load time, easier to work with load screen
