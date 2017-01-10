4e27efc1a21a4093d2b4f133d54686df61126046
ShivamDh <shdh8888@yahoo.ca>
2017-01-09 23:46:40 -0500

    New page started

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,27 @@ export default class ReactComponent extends React.Component {
   render () {
     return (
       <div>
-        <h1> Its a page about me </h1>
+        <h3 id = "AboutPg"> Meet Shivam Dharme </h3>
+		<div id = "Myself">
+			<img src = "https://raw.githubusercontent.com/ShivamDh/Personal-Website/gh-pages/4423.jpg"/>
+		</div>
+		<div id = "PointForm">
+			<p> I am a first-year student at the University of Waterloo studying Computer Engineering </p>
+			<ul> 
+				<li> Hi </li>
+			</ul>
+		</div>
+		<h1> Its a ppapage about me </h1>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
       </div>
     )
   }
