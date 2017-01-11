9161cf83c4d070bc5ad90e495059123ca390eb24
ShivamDh <shdh8888@yahoo.ca>
2017-01-10 23:53:09 -0500

    Filler page for the contact me page on website

---
+++ pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class ReactComponent extends React.Component {
+
+  render () {
+    return (
+      <div>
+        <div className="ContactPg"> <h1>Contact Me Here </h1>
+		
+		</div>
+      </div>
+    )
+  }
+}
