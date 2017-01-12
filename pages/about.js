31ed6439bd4bf7eb984d55f57f99efff2438aac7
ShivamDh <shdh8888@yahoo.ca>
2017-01-11 23:42:15 -0500

    Added new picture and altered format

--- pages/about.js
+++ pages/about.js
@@ -5,17 +5,30 @@ export default class ReactComponent extends React.Component {
   render () {
     return (
       <div>
-        <h3 id = "AboutPg"> Meet Shivam Dharme </h3>
+        <div id = "Background">
+			<img src = "http://kingofwallpapers.com/black-abstract-wallpaper/black-abstract-wallpaper-018.jpg"/>
+		</div>
+		<h3 id = "AboutPg"> MEET SHIVAM DHARME </h3>
 		<div id = "Myself">
-			<img src = "https://raw.githubusercontent.com/ShivamDh/Personal-Website/gh-pages/4423.jpg"/>
+			<img src = "https://raw.githubusercontent.com/ShivamDh/Personal-Website/gh-pages/4425.png"/>
 		</div>
-		<div id = "PointForm">
-			<p> I am a first-year student at the University of Waterloo studying Computer Engineering </p>
-			<ul> 
-				<li> Hi </li>
-			</ul>
+		<br/> <br/><br/> <br/> <br/> <br/>
+		<div id = "break"> </div>
+		<div id = "Para">
+			<p> My name is Shivam Dharme and I'm a first year university student at the University of Waterloo.
+				I am currently studying Computer Engineering and looking for my first co-op job in the Spring term of 2017.</p>
+			<p> In my short time here at Waterloo, I have been exposed to many new experiences, 
+				gaining an exceptional amount of expertise and knowledge that I hope to apply to the real-world </p>
 		</div>
-		<h1> Its a ppapage about me </h1>
+		<div id = "Skills">
+		Hi
+		</div>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
 		<br/>
 		<br/>
 		<br/>
