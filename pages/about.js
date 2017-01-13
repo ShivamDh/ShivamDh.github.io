cedfbebcf65d3ed9f24b5bef26551f612b4095ae
ShivamDh <shdh8888@yahoo.ca>
2017-01-12 22:15:24 -0500

    Shaping about page well

--- pages/about.js
+++ pages/about.js
@@ -16,12 +16,23 @@ export default class ReactComponent extends React.Component {
 		<div id = "break"> </div>
 		<div id = "Para">
 			<p> My name is Shivam Dharme and I'm a first year university student at the University of Waterloo.
-				I am currently studying Computer Engineering and looking for my first co-op job in the Spring term of 2017.</p>
-			<p> In my short time here at Waterloo, I have been exposed to many new experiences, 
-				gaining an exceptional amount of expertise and knowledge that I hope to apply to the real-world </p>
+				I am currently studying Computer Engineering and looking for my first co-op job for the Spring term of 2017.
+			</p>
+			<p> While studying here, I have also been a part of the University of Waterloo Midnight Sun Solar Car
+				Team as well as complete a few side projects on my own time. Feel free to go to the Projects tab 
+				to view any of the projects displayed.
+			</p>
+			<p>
+				My interests and passion lie in software development, specifically implementation and testing.
+				I love to use my problem solving skills to come up with creative and unique solutions.
+			</p>
+			<h4>
+				I am open to collaboration and job opportunities with my resume available here to view.
+			</h4>
 		</div>
 		<div id = "Skills">
-		Hi
+			<h1> Skillset </h1>
+			List of Skills...
 		</div>
 		<br/>
 		<br/>
