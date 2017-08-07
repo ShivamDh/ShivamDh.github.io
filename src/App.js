7e8dbb10d2ca41be6f9fdf23b58687b75152ed81
ShivamDh <shdh8888@yahoo.ca>
2017-08-06 23:54:29 -0400

    Changing links to create an SPA (Single Page App) and allow for url specific pages on static host like Github

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import PageNotFound from './PageNotFound';
 import {Navbar, Nav, NavItem} from 'react-bootstrap'
 import FlatButton from 'material-ui/FlatButton'
 import FontIcon from 'material-ui/FontIcon'
@@ -8,43 +10,61 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import ActionAndroid from 'material-ui/svg-icons/action/android'
 import IconButton from 'material-ui/IconButton';
 import ActionHome from 'material-ui/svg-icons/action/home';
+import Home from './home'
+import Contact from './contact'
+import About from './about'
+import Projects from './projects'
 
 import './App.css';
 
 class App extends Component {
-
+	
 	getClass = (url) => {
-		return url === this.props.location.pathname ? 'header-link-active' : 'header-link-inactive'
+		return url === window.location.pathname ? 'header-link-active' : 'header-link-inactive'
 	}
+	
+	render() {
+		return (
+		    <MuiThemeProvider >
+				<div className="App">
+					<Navbar inverse>
+						<Navbar.Header>
+							<Navbar.Brand>
+								<div>
+									<img className="img-responsive" data-letters="Shivam"
+										src="img/header-logo.png" alt="Website Name/Logo" />
+									<div className="name">
+										<div className="firstName"> SHIVAM </div>
+										<div className="lastName"> DHARME </div>
+									</div>
+								</div>
+							</Navbar.Brand>
+							<Navbar.Toggle />
+						</Navbar.Header>
+						<Navbar.Collapse>
+							<Nav bsClass="nav headerLinks">
+								<NavItem className={this.getClass('/')} href="/"> <span data-hover="HOME"> HOME </span> </NavItem>
+								<NavItem className={this.getClass('/about')} href="/about"> <span data-hover="ABOUT"> ABOUT </span></NavItem>
+								<NavItem className={this.getClass('/projects')} href="/projects"> <span data-hover="PROJECTS"> PROJECTS </span></NavItem>
+								<NavItem className={this.getClass('/contact')} href="/contact"> <span data-hover="CONTACT"> CONTACT </span></NavItem>
+							</Nav>
+						</Navbar.Collapse>
+					</Navbar>
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route path="/about" component={About} />
+						<Route path="/projects" component={Projects} />
+						<Route path="/contact" component={Contact} />
+						<Route component={PageNotFound} />
+					</Switch>
+					<div className="footer">
 
-  	render() {
-  		console.log(this.props.location.pathname)
-    	return (
-			<div className="App">
-				<Navbar inverse>
-					<Navbar.Header>
-						<Navbar.Brand>
-							<div className="nameLogo" data-letters="Shivam"> Shivam </div>
-						</Navbar.Brand>
-						<Navbar.Toggle />
-					</Navbar.Header>
-					<Navbar.Collapse>
-						<Nav bsClass="nav headerLinks">
-							<NavItem className={this.getClass('/')} href="/"> <span data-hover="HOME"> HOME </span> </NavItem>
-							<NavItem className={this.getClass('/about')} href="/about"> <span data-hover="ABOUT"> ABOUT </span></NavItem>
-							<NavItem className={this.getClass('/projects')} href="/projects"> <span data-hover="PROJECTS"> PROJECTS </span></NavItem>
-							<NavItem className={this.getClass('/contact')} href="/contact"> <span data-hover="CONTACT"> CONTACT </span></NavItem>
-						</Nav>
-					</Navbar.Collapse>
-				</Navbar>
-				{this.props.children}
-				<div className="footer">
+					</div>
 
 				</div>
-
-			</div>
-	    );
-  	}
+			</MuiThemeProvider>
+		);
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+import React from 'react'
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import routes from './routes';
+import App from './App';
 
-ReactDOM.render(routes, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
+
 registerServiceWorker();
 injectTapEventPlugin();
