dee4f7c01d44cf30dd16dd0da871464de8a8345a
ShivamDh <shdh8888@yahoo.ca>
2017-08-07 00:44:25 -0400

    Creating Contact Page layout

--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import TextField from 'material-ui/TextField'
+import RaisedButton from 'material-ui/RaisedButton'
 import {orange500, orange900} from 'material-ui/styles/colors'
 
 import './contact.css'
@@ -8,29 +9,60 @@ class Contact extends Component {
   	render() {
     	return (
 		    <div className="Contact">
-	    		<img src="contact-343.jpg" className="header-image img-responsive" alt="Contact Page Header"/>
-		    	<div className="contact-header">
-		    		<div className="get-in-touch">
-			    		GET IN TOUCH 
+		    	<div className="contact-header" style={{'background-image' : 'url(img/contact-header.jpg)'}}>
+			    	<div className="contact-header-text">
+			    		<div className="get-in-touch">
+				    		GET IN TOUCH 
+				    	</div>
+				    	<div className="breakLine center-block"/>
+				    	<div className="caption">
+				    		I'll always make the time to respond to any inquiries
+				    	</div>
 			    	</div>
-			    	<div className="breakLine center-block"/>
-			    	<div className="caption">
-			    		I'll always make the time to respond to any of your inquiries
-			    	</div>
-		    	</div>
+			    </div>
 		    	<div className="mainBody">
 			    	<div className="contact-info">
 			    		Contact Me
 			    	</div>
 			    	<div className="contact-form">
-			    		<div className="form-header text-center"> Send a Message </div>
-			    		<div className="form-firstName center-block">
-			    			Name:
-			    			<TextField floatingLabelText="Styled Floating Label Text"
-			    				underlineStyle={{borderColor: orange500}}
-			    				underlineFocusStyle={{borderColor: orange900}}
-			    				floatingLabelFocusStyle={{color: orange900}}
-							    floatingLabelStyle={{color: orange500}}/>
+			    		<div className="contact-form-inner center-block">
+				    		<div className="form-header text-center h1"> Send a Message </div>
+				    		<div className="form-firstName">
+				    			<TextField floatingLabelText="First Name"
+				    				inputStyle={{color: '#9F9F9F'}}
+				    				underlineStyle={{borderColor: orange500}}
+				    				underlineFocusStyle={{borderColor: orange900}}
+				    				floatingLabelFocusStyle={{color: orange900}}
+								    floatingLabelStyle={{color: orange500}}/>
+				    		</div>
+				    		<div className="form-lastName">
+				    			<TextField floatingLabelText="Last Name"
+				    				inputStyle={{color: '#9F9F9F'}}
+				    				underlineStyle={{borderColor: orange500}}
+				    				underlineFocusStyle={{borderColor: orange900}}
+				    				floatingLabelFocusStyle={{color: orange900}}
+								    floatingLabelStyle={{color: orange500}}/>
+				    		</div>
+				    		<div className="form-email">
+				    			<TextField floatingLabelText="Email"
+				    				inputStyle={{color: '#9F9F9F'}}
+				    				underlineStyle={{borderColor: orange500}}
+				    				underlineFocusStyle={{borderColor: orange900}}
+				    				floatingLabelFocusStyle={{color: orange900}}
+								    floatingLabelStyle={{color: orange500}}/>
+				    		</div>
+				    		<div className="form-message">
+				    			<TextField floatingLabelText="Message"
+				    				hintText="Enter your message"
+				    				rows="3" multiLine floatingLabelFixed
+				    				underlineStyle={{borderColor: orange500}}
+				    				underlineFocusStyle={{borderColor: orange900}}
+				    				floatingLabelFocusStyle={{color: orange900}}
+								    floatingLabelStyle={{color: orange500}}/>
+				    		</div>
+				    		<div className="form-submit">
+				    			<RaisedButton label="Submit" style={{margin: 12}} primary />
+				    		</div>
 			    		</div>
 			    	</div>
 			    </div>
--- src/home.js
+++ src/home.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import './home.css';
 
 class Home extends Component {
-  render() {
-    return (
-      <div className="home">
-        <div className="home-background" >
-        </div>
+    render() {
+        return (
+            <div className="home">
+                <div className="home-background" >
+                </div>
 
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default Home;
