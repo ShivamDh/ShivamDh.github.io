e653986b9d3303c57617df7aad039b6c16265d41
ShivamDh <shdh8888@yahoo.ca>
2017-08-28 17:07:06 -0400

    New scrollbar, editing about me page

--- src/about.js
+++ src/about.js
@@ -2,16 +2,24 @@ import React, { Component } from 'react';
 import './about.css';
 
 class About extends Component {
-  render() {
-    return (
-      <div className="Home">
-        <div className="Home-header">
-          <h2>Welcome to About </h2>
-        </div>
-		<div> Done Testing </div>
-      </div>
-    );
-  }
+
+    render() {
+        return (
+            <div className="About">
+                <div className="header">
+                    <div className="header-pic">
+                        <img src="img/44245.png" alt="Shivam Dharme"/>
+                    </div>
+                    <div className="header-text"> 
+                        <div className="text"> <i> - MEET SHIVAM DHARME - </i> </div>
+                    </div>
+                </div>
+
+                
+
+            </div>
+        )
+    }
 }
 
 export default About;
