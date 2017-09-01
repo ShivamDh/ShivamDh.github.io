c7a0a12ca8f46359f88d22a5fa91b997c6ec5841
ShivamDh <shdh8888@yahoo.ca>
2017-09-01 15:50:37 -0400

    Modifying scrollbar, creating landing page

--- src/home.js
+++ src/home.js
@@ -6,10 +6,21 @@ class Home extends Component {
         return (
             <div className="home">
                 <div className="home-background" style={{backgroundImage : 'url(img/home-background.jpg)'}}>
-
-
+                	<img className="home-profile-image" src="img/4424.jpg" alt="Profile"/>
+                	<div className="home-profile-text">
+                		<div className="home-profile-text-describ text-center">
+                			Developer
+                			<span> ∙ </span>
+                			Designer
+                			<span> ∙ </span>
+                			Innovator
+                		</div>
+                		<div className="home-profile-text-breakline" />
+                		<div className="home-profile-text-name text-center">
+                			Shivam Dharme
+                		</div>
+                	</div>
                 </div>
-
             </div>
         );
     }
