e5bdb15b587e186f1f1c1ea13f76f85248e3716f
ShivamDh <shdh8888@yahoo.ca>
2017-09-10 22:19:58 -0400

    Creating home page description

--- src/home.js
+++ src/home.js
@@ -5,7 +5,7 @@ class Home extends Component {
     render() {
         return (
             <div className="home">
-                <div className="home-background" style={{backgroundImage : 'url(img/home-background.jpg)'}}>
+                <div className="home-background" style={{backgroundImage: 'url(img/home-background.jpg)'}}>
                 	<img className="home-profile-image" src="img/4424.jpg" alt="Profile"/>
                 	<div className="home-profile-text">
                 		<div className="home-profile-text-describ text-center">
@@ -20,6 +20,20 @@ class Home extends Component {
                 			Shivam Dharme
                 		</div>
                 	</div>
+                </div>
+                <div className="home-about-me" style={{backgroundImage: 'url(img/home-about.jpg'}} >
+                    <div className="home-about-me-who">
+                        <div className="home-about-me-who-header">
+                            <span>&lt;</span>/<span>&gt;</span>
+                            &nbsp;&nbsp;&nbsp;WHO AM I&nbsp;&nbsp;&nbsp;
+                            <span>&lt;</span>/<span>&gt;</span>
+                        </div>
+                        <div className="home-about-me-who-body">
+                            I am a software developer who is <span> passionate </span> about creating <span> superior </span> applications through
+                            <span> effective </span> code and <span> efficient </span> algorithms. I like to produce <span> smart </span> and
+                            <span> cutting-edge </span> solutions to obstacles at hand to deliver <span> excellence </span> at every turn.
+                        </div>
+                    </div>
                 </div>
             </div>
         );
