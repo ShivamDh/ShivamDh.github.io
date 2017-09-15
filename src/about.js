81d8b046387081eaa53a6faa8384fee291cb6e09
ShivamDh <shdh8888@yahoo.ca>
2017-09-14 23:58:51 -0400

    More personal events to timeline

--- src/about.js
+++ src/about.js
@@ -167,6 +167,41 @@ class About extends Component {
                                 <img src="img/about-school-icon.png" alt="Study"/>
                             </div>
                         </div>
+
+                        <div className="cd-timeline-block">
+                            <div className="cd-timeline-content">
+                                <div className="cd-date"> OCT <span> 2015 </span> - MAR <span> 2016 </span> </div>
+                                <div className="cd-timeline-content-header">Board of Director and Finance Team Member</div>
+                                <div className="cd-timeline-content-subheader">
+                                    <a href="http://scentsationja.wixsite.com/scentsation"> Scentsation - A Junior Achievement Company </a> 
+                                </div> 
+                                <div className="cd-timeline-content-text">
+                                    Managed company's balances and projected return on investments (ROI) through Excel worksheets.
+                                    Assisted the production team in sourcing environmentally-friendly packaging for products.
+                                </div>
+                            </div> 
+                            <div className="cd-timeline-img">
+                                <img src="img/about-chart-icon.png" alt="Company"/>
+                            </div>
+                        </div>
+						
+						<div className="cd-timeline-block">
+                            <div className="cd-timeline-content">
+                                <div className="cd-date"> SEPT <span> 2014 </span> - JUN <span> 2016 </span> </div>
+                                <div className="cd-timeline-content-header">Youth Sports and Aquatics Volunteer</div>
+                                <div className="cd-timeline-content-subheader">
+                                    <a href="https://www.ymcacalgary.org/"> YMCA Calgary </a> 
+                                </div> 
+                                <div className="cd-timeline-content-text">
+                                    Aided swim instructors with lessons by managing large class sizes, took the initiative to run
+									class games at the end of lessons to review learned skills. 
+									Supervised weekly sports for children, responsible for the safety, setup and conduct of said events.
+								</div>
+                            </div> 
+                            <div className="cd-timeline-img">
+                                <img src="img/about-play-icon.png" alt="Company"/>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <a className="about-resume" href="Res.pdf">
