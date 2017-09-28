69bca89cfef20dfdbe36e08f7d1da6b01e3842a7
ShivamDh <shdh8888@yahoo.ca>
2017-09-27 20:51:40 -0400

    Styling for 404 page, contact page mixin implementation

--- src/PageNotFound.js
+++ src/PageNotFound.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './PageNotFound.css';
 
 const propTypes = {
   location: PropTypes.object.isRequired,
 };
 
 export default function PageNotFound({ location }) {
-  return (
-    <p>
-      Page not found - the path: {location.pathname},
-      did not match any React Router routes.
-    </p>
-  );
+	console.log('Route unmatched: ', location.pathname)
+
+  	return (
+  		<div className="pageNotFound"
+  			style={{backgroundImage: 'url(https://raw.githubusercontent.com/ShivamDh/ShivamDh.github.io/development/public/img/404-background.jpg)'}}>
+  			<div className="pageNotFoundText">
+		      	<div className="pageHeader"> Uh oh! Something went wrong there ... </div>
+		      	<div className="pageSubHeader"> Use the navigation tools from the header or footer to navigate to a valid page </div>
+	        </div>
+	    </div>
+  	);
 }
 
 PageNotFound.propTypes = propTypes;
