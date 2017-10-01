ea85d0164ff209bd14427171bcbbc84cdc18fc06
ShivamDh <shdh8888@yahoo.ca>
2017-10-01 19:05:56 -0400

    Ading preloader

---
+++ preloader.js
@@ -0,0 +1,12 @@
+function pageload() {
+    var e = (new Date).getTime(),
+        t = (e - before) / 1e3,
+        n = document.getElementById("loadingtime");
+    n.innerHTML = "Page load: " + t + " seconds."
+}
+window.onload = function() {
+}, document.addEventListener ? document.addEventListener("DOMContentLoaded", function() {
+    document.removeEventListener("DOMContentLoaded", arguments.callee, !1), domReady()
+}, !1) : document.attachEvent && document.attachEvent("onreadystatechange", function() {
+    "complete" === document.readyState && (document.detachEvent("onreadystatechange", arguments.callee), domReady())
+});
