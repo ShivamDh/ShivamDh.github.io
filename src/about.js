855f756b1cc332a0ee75d80755424e1cfd16f2e7
Shivam Dharme <shdh8888@yahoo.ca>
2018-06-03 05:44:41 -0400

    Adding more styling, cleaning up JS file

--- src/about.js
+++ src/about.js
@@ -70,7 +70,8 @@ class About extends Component {
     render() {
         return (
             <div className="About">
-                <div className="header" style={{backgroundImage: 'url(https://cdn.rawgit.com/ShivamDh/ShivamDh.github.io/development/public/img/about-header.jpg)'}}>
+                <div className="header"
+                style={{backgroundImage: 'url(https://cdn.rawgit.com/ShivamDh/ShivamDh.github.io/development/public/img/about-header.jpg)'}}>
                     <div className="header-pic">
                         <img src="https://cdn.rawgit.com/ShivamDh/ShivamDh.github.io/development/public/img/44251.png" alt="Shivam Dharme"/>
                     </div>
@@ -79,7 +80,8 @@ class About extends Component {
                     </div>
                 </div>
                 <div className="about-me">
-                    <div className="about-me-info" style={{backgroundImage: 'url(https://cdn.rawgit.com/ShivamDh/ShivamDh.github.io/development/public/img/about-info.jpg'}} >
+                    <div className="about-me-info"
+                    style={{backgroundImage: 'url(https://cdn.rawgit.com/ShivamDh/ShivamDh.github.io/development/public/img/about-info.jpg'}} >
                         <div className="about-me-info-first">
                             <span> Hi! </span>
                             I'm a second-year Computer Engineering co-op student at the <a href="https://uwaterloo.ca/"> University of Waterloo </a>.
@@ -105,7 +107,8 @@ class About extends Component {
                             passion and interest as me for software development.
                         </div>
                     </div>
-                    <div className="about-me-skills" style={{backgroundImage: 'url(https://cdn.rawgit.com/ShivamDh/ShivamDh.github.io/development/public/img/skills-background.jpg)'}} >
+                    <div className="about-me-skills"
+                        style={{backgroundImage: 'url(https://cdn.rawgit.com/ShivamDh/ShivamDh.github.io/development/public/img/skills-background.jpg)'}} >
                         <div className="about-me-skills-header"> Skills </div>
                         <div className="about-me-skills-software">
                             <div className="about-me-skills-software-header"> Software Knowledge </div>
@@ -139,8 +142,10 @@ class About extends Component {
                                     <a href="https://www.d2l.com/"> WATonomous - SAE AutoDrive Challenge </a> 
                                 </div> 
                                 <div className="cd-timeline-content-text">
-                                    - Implemented computer vision software with OpenCV and C++ to track traffic signs <br/>
-                                    - Processed and synchronized IMU data to produce output data <br/>
+                                    - Implemented computer vision software with OpenCV and C++ to track traffic signs
+                                    <br/>
+                                    - Processed and synchronized IMU data to produce output data
+                                    <br/>
                                     - Coded with Robot Operating System (ROS) on Ubuntu for environment setup and package output
                                 </div>
                             </div> 
@@ -156,9 +161,12 @@ class About extends Component {
                                     <a href="https://www.d2l.com/"> D2L (formerly Desire2Learn) </a> 
                                 </div> 
                                 <div className="cd-timeline-content-text">
-                                    - Integrated SCORM tools with existing tools for customization in e-learing using C#.NET, REST, MVC <br/>
-                                    - Implemented new features to 'Add' feature in Lessons tool for 50% more content availabilit using React, Node, MySQL <br/>
-                                    - Incorporated time tracking feature to monitor content viewing <br/>
+                                    - Integrated SCORM tools with existing tools for customization in e-learing using C#.NET, REST, MVC
+                                    <br/>
+                                    - Implemented new features to 'Add' feature in Lessons tool for 50% more content availabilit using React, Node, MySQL
+                                    <br/>
+                                    - Incorporated time tracking feature to monitor content viewing
+                                    <br/>
                                     - Created API for PostgreSQL script for data purge regarding European Union Data Protection Regulation
                                 </div>
                             </div> 
@@ -174,9 +182,12 @@ class About extends Component {
                                     <a href="http://www.evertz.com/"> Evertz Microsystems </a> 
                                 </div> 
                                 <div className="cd-timeline-content-text">
-                                    - Developed scalable website applications integrating video streams through React, Redux, Node, jQuery, LESS <br/>
-                                    - Rearchitected search engine for web component modularity, increasing efficiency by over 20% <br/>
-                                    - Enhanced website data workflow by efficient result querying and refined search results with Python <br/>
+                                    - Developed scalable website applications integrating video streams through React, Redux, Node, jQuery, LESS
+                                    <br/>
+                                    - Rearchitected search engine for web component modularity, increasing efficiency by over 20%
+                                    <br/>
+                                    - Enhanced website data workflow by efficient result querying and refined search results with Python
+                                    <br/>
                                     - Built a client-specific platform utilizing barcode recognition and job scheduling through Python and PostgreSQL
                                 </div>
                             </div> 
@@ -192,8 +203,10 @@ class About extends Component {
                                     <a href="http://www.uwmidsun.com/"> Midnight Sun Solar Race Car Team </a> 
                                 </div> 
                                 <div className="cd-timeline-content-text">
-                                    - Built a working footer module through React and Node <br/>
-                                    - Styled new icons and media links on footer with Bootstrap and SCSS <br/>
+                                    - Built a working footer module through React and Node
+                                    <br/>
+                                    - Styled new icons and media links on footer with Bootstrap and SCSS
+                                    <br/>
                                     - Wrote bare-metal firmware for car's embedded systems, in SPI and C language
                                 </div>
                             </div> 
@@ -209,7 +222,8 @@ class About extends Component {
                                     <a href="http://www.uwaterloo.ca/"> University of Waterloo </a> 
                                 </div> 
                                 <div className="cd-timeline-content-text">
-                                    - Candidate for Bachelor's of Applied Science in Computer Engineering <br/>
+                                    - Candidate for Bachelor's of Applied Science in Computer Engineering
+                                    <br/>
                                     - First year average: 87%. Top 10 Rank
                                     <strong> Expected Graduation in 2021 </strong>
                                 </div>
@@ -227,8 +241,10 @@ class About extends Component {
                                     <a href="http://scentsationja.wixsite.com/scentsation"> Scentsation - A Junior Achievement Company </a> 
                                 </div> 
                                 <div className="cd-timeline-content-text">
-                                    - Managed company's balances and projected return on investments (ROI) through Excel worksheets <br/>
-                                    - Assisted the production team in sourcing environmentally-friendly packaging for products <br/>
+                                    - Managed company's balances and projected return on investments (ROI) through Excel worksheets
+                                    <br/>
+                                    - Assisted the production team in sourcing environmentally-friendly packaging for products
+                                    <br/>
                                 </div>
                             </div> 
                             <div className="cd-timeline-img">
@@ -244,8 +260,10 @@ class About extends Component {
                                     <a href="https://www.ymcacalgary.org/"> YMCA Calgary </a> 
                                 </div> 
                                 <div className="cd-timeline-content-text">
-                                    - Aided swim instructors with lessons by managing large class sizes <br/>
-                                    - Took initiative to run class games at the end of lessons to review learned skills <br/> 
+                                    - Aided swim instructors with lessons by managing large class sizes
+                                    <br/>
+                                    - Took initiative to run class games at the end of lessons to review learned skills
+                                    <br/> 
 									- Supervised weekly sports for children, responsible for the safety, setup and conduct of said events
 								</div>
                             </div> 
